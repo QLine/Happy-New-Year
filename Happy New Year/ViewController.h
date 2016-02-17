f19b51cd66e7d62b9664c8c0990ace4d7aa5a4f8
junashi <JuNaShiMacMini@junashidemac-mini.local>
2016-02-17 10:01:54 +0800

    Initial Commit

---
+++ Happy New Year/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Happy New Year
+//
+//  Created by junashi on 16/2/17.
+//  Copyright © 2016年 junashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Happy New Year/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Happy New Year
+//
+//  Created by junashi on 16/2/17.
+//  Copyright © 2016年 junashi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
